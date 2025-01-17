59336325c4527b9d4cca50f96746f3f3d64031c6
Alexander-Jordan <curtjordan93@gmail.com>
2025-01-17 11:38:27 +0100

    Modify the player controller to make it better, and more similar to the original

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,19 +1,28 @@
 extends CharacterBody2D
 
-const JETPACK_FORCE = -15000.0
+const JETPACK_FORCE_AMPLIFIER:int = 4000
+const JETPACK_MAX_FORCE:int = 5000
+const MAX_FALL_SPEED = 1200
 
 @onready var player_animation_tree: PlayerAnimationTree = $PlayerAnimationTree
+
+var jetpack_force:float = 0
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed('fly') and jetpack_force < JETPACK_MAX_FORCE:
+		jetpack_force += JETPACK_FORCE_AMPLIFIER * delta
+	if Input.is_action_just_pressed('fly'):
+		player_animation_tree.jetpack_activated = true
+	if Input.is_action_just_released('fly'):
+		jetpack_force = 0
+		player_animation_tree.jetpack_activated = false
 
 func _physics_process(delta: float) -> void:
 	player_animation_tree.grounded = is_on_floor()
 	
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	if Input.is_action_pressed('fly'):  
-		player_animation_tree.jetpack_activated = true
-		velocity.y += (JETPACK_FORCE * delta)
-	if Input.is_action_just_released('fly'):
-		player_animation_tree.jetpack_activated = false
+	if jetpack_force > 0:
+		velocity.y = -jetpack_force
+	elif not is_on_floor() and velocity.y < MAX_FALL_SPEED:
+		velocity.y += get_gravity().y * delta
 
 	move_and_slide()
