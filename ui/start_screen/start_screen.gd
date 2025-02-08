eccba9eb17af7d0bb2878e6cf8236d73b3c865bd
Alexander-Jordan <curtjordan93@gmail.com>
2025-02-08 16:16:48 +0100

    Add coin UI to the 'menu'

--- ui/start_screen/start_screen.gd
+++ ui/start_screen/start_screen.gd
@@ -1,10 +1,17 @@
 extends Control
 
+#region VARIABLES
+## Label for displaying the total amount of collected coins.
+@onready var coins_label: Label = $VBoxContainer/MarginContainer/HBoxContainer/coins
+#endregion
+
 #region FUNCTIONS
 func _ready() -> void:
+	coins_label.text = "%d" % floori(SaveSystem.stats.coins)
 	GameManager.game_changed.connect(func(game:int):
 		match game:
 			GameManager.Game.NEW:
+				coins_label.text = "%d" % floori(SaveSystem.stats.coins)
 				visible = true
 			GameManager.Game.PLAYING:
 				visible = false
