314c8babff6904b5b2bcc9a18f004925995b1fa4
Alexander-Jordan <curtjordan93@gmail.com>
2025-02-21 15:51:03 +0100

    Add target to missile, and clean up a bit

--- entities/missile/spawner/missile_spawner.gd
+++ entities/missile/spawner/missile_spawner.gd
@@ -1,2 +1,14 @@
 class_name MissileSpawner
 extends Spawner
+## A time interval based spawner for missiles.
+
+#region VARIABLES
+## The target node to aim at.
+@export var target_node: Node2D
+#endregion
+
+#region FUNCTIONS
+func spawn() -> void:
+	spawn_point = Vector2(2000, target_node.position.y) if target_node != null else spawn_point
+	super.spawn()
+#endregion
